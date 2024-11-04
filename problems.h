ea711588f9dd2a87d72b1413ce1ae7ade9907e62
mushtariy2006 <m.zokirova@newuu.uz>
2024-11-04 12:45:24 +0500

    Add files via upload

---
+++ problems.h
@@ -0,0 +1,51 @@
+//problem1
+int floorFunction(double number) {
+     return int(number);
+}
+
+int ceilingFunction(double number) {
+    if(int(number) == number) {
+      return number;
+
+
+} else {
+  return int(number) + 1;
+}
+}
+
+void swapValues(double &a, double &b) {
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}
+int multiply(int digit1, int digit2) {
+    if (digit2 == 0) {
+        return 0;
+    }
+    if (digit2 < 0) {
+        return -(multiply(digit1, -digit2));
+    }
+    return digit1 + multiply(digit1, digit2 - 1);
+}
+int sumOfDigits(int constant) {
+    if (constant == 0) {
+        return 0;
+    }
+    return (constant % 10) + sumOfDigits(constant/ 10);
+}
+int decimalToBinary(int guy) {
+    if (guy == 0) {
+        return 0;
+    }
+    return (guy%2)+10*decimalToBinary(guy/2);
+   }
+template <typename T>
+T midValue(T a, T b, T c) {
+    if ((a >= b && a <= c)  (a <= b && a >= c)) {
+        return a;
+    } else if ((b >= a && b <= c)  (b <= a && b >= c)) {
+        return b;
+    } else {
+        return c;
+    }
+}
