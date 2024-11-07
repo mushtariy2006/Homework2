24eb897c9ea694e9bad985d1b81ceeb02d0491cb
mushtariy2006 <m.zokirova@newuu.uz>
2024-11-07 09:35:58 +0500

    Add files via upload

---
+++ problem5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    int uppercaseCount = 0, lowercaseCount = 0;
+    string uppercaseChars = "", lowercaseChars = "";
+
+    for (char c : s) {
+        if (isupper(c)) {
+            uppercaseCount++;
+            uppercaseChars += c;
+            uppercaseChars += " ";
+        } else if (islower(c)) {
+            lowercaseCount++;
+            lowercaseChars += c;
+            lowercaseChars += " ";
+        }
+    }
+
+    // Remove trailing space from strings
+    if (!uppercaseChars.empty()) uppercaseChars.pop_back();
+    if (!lowercaseChars.empty()) lowercaseChars.pop_back();
+
+    cout << uppercaseCount << " " << uppercaseChars << endl;
+    cout << lowercaseCount << " " << lowercaseChars << endl;
+
+    return 0;
+}
