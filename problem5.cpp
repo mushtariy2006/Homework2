ead3242bae4404ca97ca55aedc1eb71d017b96a6
mushtariy2006 <m.zokirova@newuu.uz>
2024-11-09 12:40:25 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
-#include <string>
+#include <cstring>  // for strlen
 using namespace std;
 
 int main() {
-    string s;
-    getline(cin, s);
+    char s[1000];  // Assume the maximum length of the input string is 999 characters
+    cin.getline(s, 1000);  // Read the input string
 
     int uppercaseCount = 0, lowercaseCount = 0;
-    string uppercaseChars = "", lowercaseChars = "";
+    char uppercaseChars[1000] = "";  // Array to store uppercase characters
+    char lowercaseChars[1000] = "";  // Array to store lowercase characters
 
-    for (char c : s) {
-        if (isupper(c)) {
+    int uppercaseIndex = 0, lowercaseIndex = 0;
+
+    // Loop through the string and classify characters
+    for (int i = 0; i < strlen(s); i++) {
+        if (isupper(s[i])) {
             uppercaseCount++;
-            if (uppercaseCount > 1) uppercaseChars += " ";  // Add space only after the first character
-            uppercaseChars += c;
-        } else if (islower(c)) {
+            uppercaseChars[uppercaseIndex++] = s[i];
+            uppercaseChars[uppercaseIndex++] = ' ';  // Add a space after the character
+        } else if (islower(s[i])) {
             lowercaseCount++;
-            if (lowercaseCount > 1) lowercaseChars += " ";  // Add space only after the first character
-            lowercaseChars += c;
+            lowercaseChars[lowercaseIndex++] = s[i];
+            lowercaseChars[lowercaseIndex++] = ' ';  // Add a space after the character
         }
     }
 
+    // Null-terminate the arrays to print them correctly
+    if (uppercaseIndex > 0) uppercaseChars[uppercaseIndex - 1] = '\0';  // Remove last space for uppercase
+    if (lowercaseIndex > 0) lowercaseChars[lowercaseIndex - 1] = '\0';  // Remove last space for lowercase
+
+    // Print results
     cout << uppercaseCount << " " << uppercaseChars << endl;
     cout << lowercaseCount << " " << lowercaseChars << endl;
 
