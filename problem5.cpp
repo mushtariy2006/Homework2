747850c9668748a3e13037536a8ecc4364ee8f91
mushtariy2006 <m.zokirova@newuu.uz>
2024-11-09 12:28:32 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 int main() {
     string s;
     getline(cin, s);
-
     int uppercaseCount = 0, lowercaseCount = 0;
     string uppercaseChars = "", lowercaseChars = "";
-
     for (char c : s) {
         if (isupper(c)) {
             uppercaseCount++;
@@ -20,13 +17,9 @@ int main() {
             lowercaseChars += " ";
         }
     }
-
-    
     if (!uppercaseChars.empty()) uppercaseChars.pop_back();
     if (!lowercaseChars.empty()) lowercaseChars.pop_back();
-
     cout << uppercaseCount << " " << uppercaseChars << endl;
     cout << lowercaseCount << " " << lowercaseChars << endl;
-
     return 0;
 }
