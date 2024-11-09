52e031590def3a0974064ae58a87da861f218612
mushtariy2006 <m.zokirova@newuu.uz>
2024-11-09 12:30:08 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main() {
     string s;
     getline(cin, s);
+
     int uppercaseCount = 0, lowercaseCount = 0;
     string uppercaseChars = "", lowercaseChars = "";
+
     for (char c : s) {
         if (isupper(c)) {
             uppercaseCount++;
+            if (uppercaseCount > 1) uppercaseChars += " ";  // Add space only after the first character
             uppercaseChars += c;
-            uppercaseChars += " ";
         } else if (islower(c)) {
             lowercaseCount++;
+            if (lowercaseCount > 1) lowercaseChars += " ";  // Add space only after the first character
             lowercaseChars += c;
-            lowercaseChars += " ";
         }
     }
-    if (!uppercaseChars.empty()) uppercaseChars.pop_back();
-    if (!lowercaseChars.empty()) lowercaseChars.pop_back();
+
     cout << uppercaseCount << " " << uppercaseChars << endl;
     cout << lowercaseCount << " " << lowercaseChars << endl;
+
     return 0;
 }
