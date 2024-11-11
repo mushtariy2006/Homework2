89c0752ce1e2e9a046c4131f06abd26e11d5d801
mushtariy2006 <m.zokirova@newuu.uz>
2024-11-11 17:17:06 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -1,37 +1,24 @@
 #include <iostream>
-#include <cstring>  // for strlen
+
 using namespace std;
 
-int main() {
-    char s[1000];  // Assume the maximum length of the input string is 999 characters
-    cin.getline(s, 1000);  // Read the input string
-
-    int uppercaseCount = 0, lowercaseCount = 0;
-    char uppercaseChars[1000] = "";  // Array to store uppercase characters
-    char lowercaseChars[1000] = "";  // Array to store lowercase characters
-
-    int uppercaseIndex = 0, lowercaseIndex = 0;
-
-    // Loop through the string and classify characters
-    for (int i = 0; i < strlen(s); i++) {
-        if (isupper(s[i])) {
-            uppercaseCount++;
-            uppercaseChars[uppercaseIndex++] = s[i];
-            uppercaseChars[uppercaseIndex++] = ' ';  // Add a space after the character
-        } else if (islower(s[i])) {
-            lowercaseCount++;
-            lowercaseChars[lowercaseIndex++] = s[i];
-            lowercaseChars[lowercaseIndex++] = ' ';  // Add a space after the character
-        }
-    }
-
-    // Null-terminate the arrays to print them correctly
-    if (uppercaseIndex > 0) uppercaseChars[uppercaseIndex - 1] = '\0';  // Remove last space for uppercase
-    if (lowercaseIndex > 0) lowercaseChars[lowercaseIndex - 1] = '\0';  // Remove last space for lowercase
-
-    // Print results
-    cout << uppercaseCount << " " << uppercaseChars << endl;
-    cout << lowercaseCount << " " << lowercaseChars << endl;
+int main()
+{
+    string s;
+    cin>>s;
+    string upper="",lower="";
+    for(auto x:s)
+        if(isupper(x))
+            upper+=x;
+        else
+            lower+=x;
+    cout<<upper.length()<<" ";
+    for(auto x:upper)
+        cout<<x<<" ";
+    cout<<endl<<lower.length()<<" ";
+    for(auto x:lower)
+        cout<<x<<" ";
+    
 
     return 0;
 }
