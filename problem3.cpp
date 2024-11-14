599982b6b12a56191e4916817dc3d0a013727bcb
mushtariy2006 <m.zokirova@newuu.uz>
2024-11-14 16:30:50 +0500

    Add files via upload

--- problem3.cpp
+++ problem3.cpp
@@ -3,25 +3,43 @@
 
 using namespace std;
 
-int findSingleNumber(const vector<int>& nums) {
-    int single_number = 0;
-    for (int num : nums) {
-        single_number ^= num; // XOR operation
-    }
-    return single_number;
-}
-
 int main() {
-    int n;
-    cin >> n;
+    int m, n, t;
+    cout<<"m=";
+    cin>>m;
+    cout<<"n=";
+    cin>>n;
+    // Input number of rows, columns, and number of modifications
 
-    vector<int> nums(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> nums[i];
+
+    // Initialize the matrix with size m x n
+    vector<vector<int>> matrix(m, vector<int>(n));
+
+    // Input the matrix elements
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+    cout<<"t=";
+    cin>>t;
+
+    // Process the modification queries
+    for (int i = 0; i < t; ++i) {
+        int row, col, value;
+        cin >> row >> col >> value;
+
+        // Convert from 1-based index to 0-based index for the matrix
+        matrix[row - 1][col - 1] = value;
     }
 
-    int result = findSingleNumber(nums);
-    cout << result << endl;
+    // Output the modified matrix
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
