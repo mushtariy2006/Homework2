ea78d666ab80aa73e34d864b9c9aa6787d2a89fb
mushtariy2006 <m.zokirova@newuu.uz>
2024-12-06 16:50:29 +0500

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int m, n;
-    cin >> m >> n;
-    int matrix[m][n];
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin >> matrix[i][j];
-        }
-    }
-    for (int i = 0; i < m; ++i) {
-        int rowSum = 0;
-        for (int j = 0; j < n; ++j) {
-            rowSum += matrix[i][j];
-        }
-        cout <<  rowSum << endl;
-    }
 
-    return 0;
-}
+
+int main() {
+   int a;
+   cin >> a;
+   for(int i=2; i<((a+1)/2); i++){
+       if (a%i) continue;
+       cout << "Not Prime";
+       return 0;
+   }
+   cout << "Prime";
