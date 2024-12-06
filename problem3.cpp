b11b29983f30e00d7cde2d88323c49f9c5e3a400
mushtariy2006 <m.zokirova@newuu.uz>
2024-12-06 16:52:27 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -1,45 +1,39 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-int main() {
-    int m, n, t;
-    cout<<"m=";
-    cin>>m;
-    cout<<"n=";
-    cin>>n;
-    // Input number of rows, columns, and number of modifications
-
-
-    // Initialize the matrix with size m x n
-    vector<vector<int>> matrix(m, vector<int>(n));
-
-    // Input the matrix elements
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin >> matrix[i][j];
-        }
-    }
-    cout<<"t=";
-    cin>>t;
-
-    // Process the modification queries
-    for (int i = 0; i < t; ++i) {
-        int row, col, value;
-        cin >> row >> col >> value;
-
-        // Convert from 1-based index to 0-based index for the matrix
-        matrix[row - 1][col - 1] = value;
-    }
-
-    // Output the modified matrix
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    return 0;
+int sumEvenNumbers(vector<int> v){
+   int i = 0, sum = 0;
+   while (v[i] != -1){
+       sum += ((v[i]%2) ? 0 : v[i]);
+       i++;
+   }
+   return sum;
 }
+
+int longestConsecutiveSubsequence(vector<int> v){
+   int i = 0, length = 0, llength = 0, number = -1;
+   while (v[i] != -1){
+       if(number != v[i]){
+           length = 1;
+           number = v[i];
+       } else {
+           length++;
+           llength = max(llength, length);
+       }
+       i++;
+   }
+   return llength;
+}
+
+
+int main() {
+   vector<int> v;
+   int n;
+   while (true) {
+       cin>>n;
+       v.push_back(n);
+       if (n==-1) break;
+   }
+   cout << "Sum of even numbers: " << sumEvenNumbers(v) << endl;
+   cout << "Length of longest consecutive subsequence: " << longestConsecutiveSubsequence(v) << endl;
